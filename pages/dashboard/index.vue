7a0ae332937ed0250fc03a6bd92138e46e027622
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-18 23:49:39 +0530

    1/3 logic done

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
     <DashHeader />
-     <div v-for="Category, index in BookCatStore.categoryData">
-      <NuxtLink :to="`${Category.categoryPath}`">
-      {{ Category.categoryName }}
+    <div
+      v-for="(Category, index) in BookCatStore.categoryData"
+      class="p-5 md:p-20"
+    >
+      <NuxtLink :to="`/dashboard/${Category.categoryName}`">
+        <UButton
+          :label="Category.categoryName"
+          :key="index"
+          variant="outline"
+          class="h-20 w-20 text-center text-xl rounded-tr-3xl"
+        />
       </NuxtLink>
-     </div>
-    <pre>{{ BookCatStore.categoryData }}</pre>
+    </div>
+    <!-- <pre>{{ BookCatStore.categoryData }}</pre> -->
   </div>
 </template>
 
 <script lang="ts" setup>
-import DashHeader from '~/components/dasboard/DashHeader.vue';
-import {useGetBookmarksCategoryStore} from '~/stores/useGetBookmarksCategoryStore'
-const BookCatStore = useGetBookmarksCategoryStore()
-
-
-
+import DashHeader from "~/components/dasboard/DashHeader.vue";
+import { useGetBookmarksCategoryStore } from "~/stores/useGetBookmarksCategoryStore";
+const BookCatStore = useGetBookmarksCategoryStore();
 </script>
 
-<style>
-
-</style>
+<style></style>
